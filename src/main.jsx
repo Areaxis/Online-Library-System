118dd92463e31ceebedc3b145b7e8e6c93881797
Areaxis <areaxismygod@gmail.com>
2026-01-19 20:15:32 +0530

    Modified main.jsx to have routers and providers, for subscribing to the redux store

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,44 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
 
-createRoot(document.getElementById('root')).render(
+import App from "./App";
+import Home from "./pages/Home";
+import BrowseBooks from "./pages/BrowseBooks";
+import BookDetails from "./pages/BookDetails";
+import AddBook from "./pages/AddBook";
+import NotFound from "./components/NotFound";
+import Layout from "./components/Layout";
+import appStore from "./utils/appStore";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { 
+        index: true, element: <Home />
+      },
+      {
+        element: <Layout />,   // sidebar
+        children: [
+          { path: "books/:category", element: <BrowseBooks /> },
+          { path: "add", element: <AddBook /> },
+          { path: "book/:id", element: <BookDetails /> }
+        ]
+      }
+    ],
+    errorElement: <NotFound />
+  }
+]);
+
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={appStore}>
+      <RouterProvider router={router} />
+    </Provider>
+  </StrictMode>
+);
